5c3ccf130c522d1e33c7c10a586757823c19464f
parkson-tano <tanoparksonsilencer@gmail.com>
2024-04-14 13:26:52 +0100

    Remove SocialIcon component from app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
         />
       </section>
 
-      <SocialIcon url="www.vimeo.com" />
       <section className="bg-gray-100 px-5 mb-5 text-slate-900">
         <div className="container mx-auto">
           <h2 className="text-3xl font-bold text-center mb-8">What You Will Learn</h2>
